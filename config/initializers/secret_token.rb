853566028c3a822a80c8ba74c653aefbb8510d1c
Jeremy Green <jeremy@octolabs.com>
2013-01-10 23:51:34 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simpledevise::Application.config.secret_token = '61560d0f8f9c3a598b229d451883f7c3ff8fb36bc8d552cc90c4cee8648b8615ca5330ef71528c3f5d5f8060032a65a8c9272e9ad6a134fd88a9033f748008e4'
