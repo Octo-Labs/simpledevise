26ecbdff66f64ec1068096c029d397c91210f86f
Jeremy Green <jeremy@octolabs.com>
2013-02-03 23:41:48 -0600

    Added a scenario for email address already taken.

--- features/signup.feature
+++ features/signup.feature
@@ -5,5 +5,11 @@ So that I can login
  
 Scenario: Sucessful sign up
   Given a user without an account
-  When he creates a new account
+  When he creates a new account with email "jeremy@octolabs.com"
   Then he should see "Welcome back, trusted user!"
+
+Scenario: Email already taken
+  Given an existing user with email "jeremy@octolabs.com"
+  Given a user without an account
+  When he creates a new account with email "jeremy@octolabs.com"
+  Then he should see "Email has already been taken"
--- features/step_definitions/signup.rb
+++ features/step_definitions/signup.rb
@@ -2,9 +2,9 @@ Given /^a user without an account$/ do
   # This condition doesn't require any explicit setup
 end
 
-When /^he creates a new account$/ do
+When /^he creates a new account with email "(.*?)"$/ do |email|
   visit "/users/sign_up"
-  fill_in('Email address', :with => 'jeremy@octolabs.com')
+  fill_in('Email address', :with => email)
   fill_in('Password', :with => 'sometestpassword')
   fill_in('Password confirmation', :with => 'sometestpassword')
   click_button('Sign up')
@@ -12,4 +12,8 @@ end
 
 Then /^he should see "(.*?)"$/ do |text|
   page.should have_content(text)
+end
+
+Given /^an existing user with email "(.*?)"$/ do |email|
+  User.create! :email => email, :password => "testpass", :password_confirmation => "testpass"
 end
